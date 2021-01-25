1d08c2eaece36633766e4aade82d094aa9d7359c
Nick <nicholas.a.robison@omb.eop.gov>
2021-01-25 14:58:48 -0500

    Add timingsafe_bcmp to c library build.
    Move the linking flags back up to the ffi library level.

--- libmacaroons/config/discover.ml
+++ libmacaroons/config/discover.ml
@@ -68,6 +68,6 @@ let () =
       let link_flags = link_flags c in
       C.Flags.write_lines "cflags" lines;
       C.Flags.write_lines "ctypes-cflags" [cstubs_cflags];
-      C.Flags.write_lines "c_library_flags" link_flags;
+      C.Flags.write_sexp "c_library_flags.sexp" link_flags;
       define_constants c;
     )
